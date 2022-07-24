3183797cb1d17242af5dc1434b079faf6d345506
sunwoo <codingbreak@naver.com>
2022-07-24 20:20:15 +0900

    first commit from master

---
+++ src/main/java/com/example/demo/controller/CommonController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.View;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class CommonController {
+
+    @GetMapping("/hello")
+    public String hello(HttpServletRequest req, @RequestParam("userName") String userName) {
+
+        log.info(">> " + userName + " << 요청이 들어왔습니다.");
+
+        return "hello";
+    }
+}
